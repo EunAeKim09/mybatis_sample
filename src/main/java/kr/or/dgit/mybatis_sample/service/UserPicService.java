4dfdd135d353d9bc1b26a5f32f491efeaf98c9f7
EunAe Kim <eunae0474@naver.com>
2018-02-13 17:20:38 +0900

    userPic api

--- src/main/java/kr/or/dgit/mybatis_sample/service/UserPicService.java
+++ src/main/java/kr/or/dgit/mybatis_sample/service/UserPicService.java
@@ -2,10 +2,36 @@ package kr.or.dgit.mybatis_sample.service;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.mybatis_sample.dto.UserPic;
+import kr.or.dgit.mybatis_sample.util.MyBatisSqlSessionFactory;
 
 public class UserPicService {
 	private static final Log log = LogFactory.getLog(UserPicService.class);
 	
 	private static String namespace = "kr.or.dgit.mybatis_sample.dao.UserPicDao.";
 	
-}
+	public int insertUserPicWithAPI(UserPic userPic) {
+		log.debug("insertUserPicWithAPI()");
+		SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();
+		int res=-1;
+		try{
+			res=sqlSession.insert(namespace+"insertUserPicWithAPI", userPic);
+			sqlSession.commit();
+		}catch (Exception e) {
+			sqlSession.rollback();
+			e.printStackTrace();
+			throw new RuntimeException(e.getCause());
+		}finally {
+			sqlSession.close();
+		}
+		return res;
+	}
+	
+	public UserPic getUserPicWithAPI(int id) {
+		try(SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();){
+			return sqlSession.selectOne(namespace+"getUserPicWithAPI", id);
+		}
+	}
+}	
