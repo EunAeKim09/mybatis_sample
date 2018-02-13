3318d96623518edbd06366d3de74abdcc0dd8c37
EunAe Kim <eunae0474@naver.com>
2018-02-13 10:42:27 +0900

    tutor, course dao dto 생성, SelectTutorByTutorIdWithAPI 생성

---
+++ src/main/java/kr/or/dgit/mybatis_sample/dto/Course.java
@@ -0,0 +1,54 @@
+package kr.or.dgit.mybatis_sample.dto;
+
+import java.util.Date;
+
+public class Course {
+	private int courseId;
+	private String name;
+	private String description;
+	private Date start_date;
+	private Date end_date;
+	private int tutor_id;
+	
+	public int getCorse_id() {
+		return courseId;
+	}
+	public void setCorse_id(int corse_id) {
+		this.courseId = corse_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public int getTutor_id() {
+		return tutor_id;
+	}
+	public void setTutor_id(int tutor_id) {
+		this.tutor_id = tutor_id;
+	}
+	@Override
+	public String toString() {
+		return String.format("Course [%s, %s, %s, %s, %s, %s]",
+				courseId, name, description, start_date, end_date, tutor_id);
+	}
+}
---
+++ src/test/java/kr/or/dgit/mybatis_sample/TutorServiceTest.java
@@ -0,0 +1,36 @@
+package kr.or.dgit.mybatis_sample;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import kr.or.dgit.mybatis_sample.dto.Tutor;
+import kr.or.dgit.mybatis_sample.service.TutorService;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TutorServiceTest {
+	private static TutorService service;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		service = new TutorService();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		service = null;
+	}
+
+	@Test
+	public void test1SelectTutorByTutorIdWithAPI() {
+		Tutor tutor = new Tutor();
+		tutor.setTutorId(1);
+		
+		Tutor findTutor = service.selectTutorByTutorIdWithAPI(tutor);
+		Assert.assertEquals(tutor.getTutorId(), 1);
+	}
+
+}
