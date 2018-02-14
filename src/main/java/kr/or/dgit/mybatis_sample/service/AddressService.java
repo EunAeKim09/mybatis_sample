5107e5baaf15cbc847ad6978d4e09f4e31d46c3b
EunAe Kim <eunae0474@naver.com>
2018-02-14 10:50:26 +0900

    row 바운드 address 검색

---
+++ src/main/java/kr/or/dgit/mybatis_sample/service/AddressService.java
@@ -0,0 +1,24 @@
+package kr.or.dgit.mybatis_sample.service;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.mybatis_sample.dao.AddressDao;
+import kr.or.dgit.mybatis_sample.dto.Address;
+import kr.or.dgit.mybatis_sample.util.MyBatisSqlSessionFactory;
+
+public class AddressService {
+	private static final Log log = LogFactory.getLog(AddressService.class);
+	
+	public List<Address> selectAddressByAll(RowBounds rowBounds) {
+		log.debug("selectAddressByAll()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			AddressDao addressDao = sqlSession.getMapper(AddressDao.class);
+			return addressDao.selectAddressByAll(rowBounds);
+		}
+	}
+}
